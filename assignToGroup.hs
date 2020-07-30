3e18becdc9d8e1ac24c70d697b728db04c5eec81
aryan <aryanmaurya1@outlook.com>
2020-07-30 18:14:27 +0530

    DS Practice

---
+++ assignToGroup.hs
@@ -0,0 +1,7 @@
+module AssignToGroup where
+
+assignToGroups n aList = zip groups aList
+                         where 
+                            groups = cycle [1..n]
+
+groups =  assignToGroups 3 ["file1.txt","file2.txt","file3.txt" ,"file4.txt","file5.txt","file6.txt","file7.txt","file8.txt"]
