386a08b209c969497afdb7cdbfcdb7e6e30d0506
aryan <aryanmaurya1@outlook.com>
2020-08-19 20:56:53 +0530

    linked list added, fixed type signature of treeSum function.

--- DataStructuresAndAlgorithms/binaryTree.hs
+++ DataStructuresAndAlgorithms/binaryTree.hs
@@ -5,8 +5,6 @@ data BinTree a =  Empty
                 | NodeBT a (BinTree a) (BinTree a)
                 deriving Show
 
-tree = NodeBT 5 (NodeBT 8 (NodeBT 3 Empty Empty) (NodeBT 1 Empty Empty)) (NodeBT 6 Empty (NodeBT 4 Empty Empty))
-
 depth :: BinTree a -> Int
 depth Empty = 0;
 depth (NodeBT _ left right) = 1 + max (depth left) (depth right)
@@ -15,6 +13,8 @@ countEmpty :: BinTree a -> Int
 countEmpty Empty = 1
 countEmpty (NodeBT _ left right) = countEmpty left + countEmpty right
 
-treeSum :: BinTree Integer -> Integer
+treeSum :: Num a => BinTree a -> a
 treeSum Empty = 0
 treeSum (NodeBT a left right) = a + treeSum left + treeSum right
+
+tree = NodeBT 5 (NodeBT 8 (NodeBT 3 Empty Empty) (NodeBT 1 Empty Empty)) (NodeBT 6 Empty (NodeBT 4 Empty Empty))
