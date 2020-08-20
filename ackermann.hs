28d823b0e82a372da3af45ef8e01dc541c37b2cb
aryan <aryanmaurya1@outlook.com>
2020-08-20 19:24:13 +0530

    refactored folder

--- ackermann.hs
+++
@@ -1,7 +0,0 @@
-module Ackermann where
--- Ackermann function
-
-ackermann :: Int -> Int -> Int
-ackermann m 0 = ackermann (m - 1) 1
-ackermann 0 n = n + 1
-ackermann m n = ackermann (m - 1) (ackermann m (n - 1))
--- collatz.hs
+++
@@ -1,9 +0,0 @@
-module Collatz where
--- Collatz conjecture
-
-collatz ::Int -> Int
-collatz 1 = 1
-collatz n = if even n 
-            then 1 + collatz (n `div` 2)
-            else 1 + collatz (n * 3 + 1)
-
